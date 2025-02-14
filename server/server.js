5e09ad4977417c090522d04b14893cfcce5a3af8
김태희 <163955023+kittyyakki@users.noreply.github.com>
2025-02-14 13:16:35 +0900

    feat:(로그인세트 만들기)

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// 미들웨어 설정
+app.use(cors());
+app.use(express.json()); // JSON 파싱
 
 app.get("/", (req, res) => {
-  res.send("🚀 서버가 정상적으로 실행 중!");
+  res.send("🚀 서버가 정상적으로 실행 중입니다!");
 });
 
-const PORT = 5000;
+// 서버 실행
 app.listen(PORT, () => {
   console.log(`✅ 서버 실행 중: http://localhost:${PORT}`);
 });
